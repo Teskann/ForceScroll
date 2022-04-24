051e719623ba764b4243dda4442f5a0ee576ae13
teskann <teskann.dev@gmail.com>
2022-04-24 15:06:22 +0200

    Initial commit

---
+++ background.js
@@ -0,0 +1,6 @@
+var r = 'html,body{overflow:auto !important;}';
+var s = document.createElement('style');
+s.type = 'text/css';
+s.appendChild(document.createTextNode(r));
+document.body.appendChild(s);
+void 0;
