4c5ba16ab8dd5d717798545b4c7a5e95b7b03f10
teskann <teskann.dev@gmail.com>
2022-04-24 16:21:54 +0200

    Update

--- background.js
+++
@@ -1,6 +0,0 @@
-var r = 'html,body{overflow:auto !important;}';
-var s = document.createElement('style');
-s.type = 'text/css';
-s.appendChild(document.createTextNode(r));
-document.body.appendChild(s);
-void 0;
